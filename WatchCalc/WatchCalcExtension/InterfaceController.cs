bb37268a3a43c8dc82653d6fd9c7d9c77af6f933
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-01-20 19:45:18 -0800

    [watchcalc] just for fun

---
+++ WatchCalc/WatchCalcExtension/InterfaceController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+using WatchKit;
+using Foundation;
+
+namespace WatchCalcExtension
+{
+	public partial class InterfaceController : WKInterfaceController
+	{
+		string displayText = "";
+
+
+		public InterfaceController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void Awake (NSObject context)
+		{
+			base.Awake (context);
+
+			// Configure interface objects here.
+			Console.WriteLine ("{0} awake with context", this);
+		}
+
+		public override void WillActivate ()
+		{
+			// This method is called when the watch view controller is about to be visible to the user.
+			Console.WriteLine ("{0} will activate", this);
+		}
+
+		public override void DidDeactivate ()
+		{
+			// This method is called when the watch view controller is no longer visible to the user.
+			Console.WriteLine ("{0} did deactivate", this);
+		}
+
+		partial void button0 () {
+			displayText += "0";
+			display.SetText(displayText);
+		}
+		partial void button1 () {
+			displayText += "1";
+			display.SetText(displayText);
+		}
+		partial void button2 () {
+			displayText += "2";
+			display.SetText(displayText);
+		}
+		partial void button3 () {
+			displayText += "3";
+			display.SetText(displayText);
+		}
+		partial void button4 () {
+			displayText += "4";
+			display.SetText(displayText);
+		}
+		partial void button5 () {
+			displayText += "5";
+			display.SetText(displayText);
+		}
+		partial void button6 () {
+			displayText += "6";
+			display.SetText(displayText);
+		}
+		partial void button7 () {
+			displayText += "7";
+			display.SetText(displayText);
+		}
+		partial void button8 () {
+			displayText += "8";
+			display.SetText(displayText);
+		}
+		partial void button9 () {
+			displayText += "9";
+			display.SetText(displayText);
+		}
+	}
+}
+
---
+++ WatchCalc/WatchCalcExtension/InterfaceController.designer.cs
@@ -0,0 +1,62 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace WatchCalcExtension
+{
+	[Register ("InterfaceController")]
+	partial class InterfaceController
+	{
+		[Outlet]
+		WatchKit.WKInterfaceLabel display { get; set; }
+
+		[Action ("button0")]
+		partial void button0 ();
+
+		[Action ("button1")]
+		partial void button1 ();
+
+		[Action ("button2")]
+		partial void button2 ();
+
+		[Action ("button3")]
+		partial void button3 ();
+
+		[Action ("button4")]
+		partial void button4 ();
+
+		[Action ("button5")]
+		partial void button5 ();
+
+		[Action ("button6")]
+		partial void button6 ();
+
+		[Action ("button7")]
+		partial void button7 ();
+
+		[Action ("button8")]
+		partial void button8 ();
+
+		[Action ("button9")]
+		partial void button9 ();
+
+		[Action ("buttonPlus")]
+		partial void buttonPlus ();
+
+		[Action ("buttonPoint")]
+		partial void buttonPoint ();
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (display != null) {
+				display.Dispose ();
+				display = null;
+			}
+		}
+	}
+}
