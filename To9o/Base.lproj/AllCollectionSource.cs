9e9350f952f91f020df4b169c9884ef6f9a432a1
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-09-17 16:47:26 -0700

    [to9o] add collectionview

---
+++ To9o/Base.lproj/AllCollectionSource.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UIKit;
+
+namespace StoryboardTables
+{
+	public class AllCollectionSource : UICollectionViewSource
+	{
+		public AllCollectionSource ()
+		{
+		}
+
+		// ##
+		// there is NO database or storage of Tasks in this example, just an in-memory List<>
+		// refer to the other Tasky samples on github for an implementation using SQLite-NET
+		// ##
+		Task[] tableItems;
+		string cellIdentifier = "todocell";
+
+		public AllCollectionSource (Task[] items)
+		{
+			tableItems = items; 
+		}
+		public override UICollectionViewCell GetCell (UICollectionView collectionView, Foundation.NSIndexPath indexPath)
+		{
+			// in a Storyboard, Dequeue will ALWAYS return a cell, 
+			var cell = collectionView.DequeueReusableCell (cellIdentifier, indexPath) as AllViewCell;
+
+			cell.Name.Text = tableItems[indexPath.Row].Name;
+
+//			if (tableItems[indexPath.Row].Done) 
+//				cell.Accessory = UITableViewCellAccessory.Checkmark;
+//			else
+//				cell.Accessory = UITableViewCellAccessory.None;
+
+			return cell;
+		}
+
+		public override nint GetItemsCount (UICollectionView collectionView, nint section)
+		{
+			return tableItems.Length;
+		}
+		public override nint NumberOfSections (UICollectionView collectionView)
+		{
+			return 1;
+		}
+
+		public Task GetItem(int id) {
+			return tableItems[id];
+		}
+	}
+}
+
---
+++ To9o/Base.lproj/AllViewCell.designer.cs
@@ -0,0 +1,26 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace StoryboardTables
+{
+	[Register ("AllViewCell")]
+	partial class AllViewCell
+	{
+		[Outlet]
+		UIKit.UILabel TodoName { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (TodoName != null) {
+				TodoName.Dispose ();
+				TodoName = null;
+			}
+		}
+	}
+}
