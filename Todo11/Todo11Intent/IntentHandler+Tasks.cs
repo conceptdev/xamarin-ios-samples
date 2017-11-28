4ce106aa5ac2a634b56e3906288bb201fc3a60e0
Craig Dunn <craig.dunn@conceptdevelopment.net>
2017-11-27 18:30:23 -0800

    [todo11] simplify siri code for demo

--- Todo11/Todo11Intent/IntentHandler+Tasks.cs
+++ Todo11/Todo11Intent/IntentHandler+Tasks.cs
@@ -18,37 +18,52 @@ namespace Todo11Intent
         /// Handles the simple phrase of adding a task (no 'list' needs to be specified)
         /// </summary>
         /// <remarks>
-        /// "<app> add <task>"
+        /// For example:
+        /// "[app] add [task]"
+        /// "Todo11 add Boeing factory tour"
         /// </remarks>
         public void HandleAddTasks(INAddTasksIntent intent, Action<INAddTasksIntentResponse> completion)
         {
-            Console.WriteLine("Add a task");
+            // 1. get the parsed data from Siri
+            var item = intent.TaskTitles[0].ToString();
+
+            // 2. have to create the tasks... in your app data store
+            SaveToApp(item);
+
+            // 3. after adding to the datastore, respond to the user with an answer/card
             var userActivity = new NSUserActivity("INAddTasksIntent"); // https://developer.apple.com/documentation/sirikit/resolving_and_handling_intents/confirming_the_details_of_an_intent
-
-            var tasklist = TaskList.FromIntent(intent);
-
-            //if tasks array is empty but a group name is specified, maybe convert it into a task?
-
-            // TODO: have to create the list and tasks... in your app data store
-
-
-            // after adding to the datastore, respond to the user with an answer/card
             var response = new INAddTasksIntentResponse(INAddTasksIntentResponseCode.Success, userActivity)
             {   // tells the user what we did
-                AddedTasks = tasklist.Tasks,
+                AddedTasks = TaskList.FromIntent(intent).Tasks,
             };
-            //if (intent.TargetTaskList != null)
-            //{
-            //    response.ModifiedTaskList = tasklist;
-            //}
             completion(response);
+        }
+
+        private static void SaveToApp(string firstTask)
+        {
+            Console.WriteLine($"'{firstTask}' was requested");
+
+            var FileManager = new NSFileManager();
+            var appGroupContainer = FileManager.GetContainerUrl("group.co.conceptdev.todo");
+            if (appGroupContainer == null)
+            {
+                Console.WriteLine("APP GROUP ERROR, probably bug #59379");
+            }
+            else
+            {
+                var appGroupContainerPath = appGroupContainer.Path;
+                Console.WriteLine("agcpath: " + appGroupContainerPath);
+                var path = System.IO.Path.Combine(appGroupContainerPath, "siri.txt");
+                Console.WriteLine("path: " + path);
+                System.IO.File.WriteAllText(path, firstTask);
+            }
         }
 
         /// <summary>
         /// Handles the create task list.
         /// </summary>
         /// <remarks>
-        /// "Make a grocery list with apples, bananas, and pears in TasksNotes"
+        /// "Make a grocery list with apples, bananas, and pears in Todo11"
         /// </remarks>
         public void HandleCreateTaskList(INCreateTaskListIntent intent, Action<INCreateTaskListIntentResponse> completion)
         {
@@ -68,7 +83,7 @@ namespace Todo11Intent
         /// Handles the set task attribute.
         /// </summary>
         /// <remarks>
-        /// "Mark buy iPhone as completed in TasksNotes"
+        /// "Mark buy iPhone as completed in Todo11"
         /// </remarks>
         public void HandleSetTaskAttribute(INSetTaskAttributeIntent intent, Action<INSetTaskAttributeIntentResponse> completion)
         {
